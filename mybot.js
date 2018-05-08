f3605e4a8f897f023ce51b98d3e2ffcd6e162348
yiplee <guoyinl@gmail.com>
2018-05-09 00:27:51 +0800

    Add: pub sub

--- mybot.js
+++ mybot.js
@@ -1,6 +1,10 @@
-const { Wechaty, Room, Contact, config } = require('wechaty')
+// import { Wechaty, Room, Contact, config, Message } from "wechaty"
+const { Wechaty, Room ,Contact, config, Message, MsgType} = require('wechaty')
 
 bot = Wechaty.instance({ profile: config.default.DEFAULT_PROFILE })
+
+var queue = 'stargazers'
+var open = require('amqplib').connect('amqp://localhost');
 
 bot
 	.on('scan', (url, code) => {
@@ -12,24 +16,36 @@ bot
 
 	.on('login', (user) => {
         console.log(`${user} login`)
-        
-        var amqp = require('amqplib/callback_api')
-        amqp.connect('amqp://localhost', function(err, conn) {
-            conn.createChannel(function(err, ch) {
-            var q = 'stargazers'
-            ch.assertQueue(q, {durable: false})
-            ch.consume(q, async function(msg) {
-                let obj = JSON.parse(msg.content.toString())
-                let name = obj.name
-                let content = obj.msg
-                if (name.length > 0 && content.length > 0) {
-                    let receiver = await Contact.find({name:name})
-                    if (receiver) {
-                        receiver.say(content)
+
+        open.then(function(conn) {
+            return conn.createChannel();
+          }).then(function(ch) {
+            return ch.assertQueue(queue,{durable: false}).then(function(ok) {
+              return ch.consume(queue, async function(msg) {
+                if (msg !== null) {
+                    let obj = JSON.parse(msg.content.toString())
+                    let content = obj.msg
+                    let from = obj.from
+                    let room = obj.room
+
+                    if (content) {
+                        if (from) {
+                            let receiver = await Contact.find({name:from})
+                            if (receiver) {
+                                receiver.say(content)
+                            }
+                        } else if (room) {
+                            let receiver = await Room.find({topic:room})
+                            if (receiver) {
+                                receiver.say(content)
+                            }
+                        }
                     }
+                    ch.ack(msg)
                 }
-            }, {noAck: true})})
-        })
+              });
+            });
+          }).catch(console.warn);
 	})
 
 	.on('friend', async function (contact, request) {
@@ -43,13 +59,29 @@ bot
 		const content = m.content()
 		const room = m.room()
 
-		if (m.self()) {
+		if (m.self() || m.type() != MsgType.TEXT) {
 			return
-		}
+        }
+        
+        const payload = {
+            "msg":content,
+            "from":contact.name(),
+        }
 
-		if (/ping/.test(content)) {
-			m.say("pong")
-		}
+        if (room) {
+            payload.room = room.topic()
+        }
+
+        message = JSON.stringify(payload)
+        console.log(message)
+
+        open.then(function(conn) {
+            return conn.createChannel()
+        }).then(function(ch) {
+            return ch.assertQueue(queue,{durable: false}).then(function(ok) {
+              return ch.sendToQueue(queue, new Buffer.from(message,'utf-8'))
+            })
+        }).catch(console.warn)
 	})
 
     .start()
